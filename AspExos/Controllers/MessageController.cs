1347d3511a081aba4dc577b1b18768383818bb8c
Samuel CHAPEL <schapel@diginamic-formation.fr>
2024-01-03 17:23:50 +0100

    Up to Exo2.9

--- AspExos/Controllers/MessageController.cs
+++ AspExos/Controllers/MessageController.cs
@@ -2,21 +2,59 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspExos.Controllers;
-public class MessageController : Controller
+
+public class MessageController(ILogger<MessageController> logger) : Controller
 {
+    private static readonly List<Message> messages = [];
+
+    private readonly ILogger<MessageController> _logger = logger;
+
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
     }
 
-    [HttpPost]
-    public IActionResult SendForm(string emetteur, string contenu, DateOnly date)
+    [HttpGet]
+    public IActionResult Form()
     {
         return View();
     }
 
     [HttpPost]
-    public IActionResult SendMessageForm(Message message)
+    public IActionResult Form(string emetteur, string contenu, DateOnly date)
+    {
+        var message = new Message(emetteur, contenu, date);
+        messages.Add(new Message(emetteur, contenu, date));
+        _logger.LogInformation($"Message ajouté : {message}");
+
+        return RedirectToAction("Form");
+    }
+
+    [HttpGet]
+    public IActionResult FormMessage()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult MessageForm(Message message)
+    {
+        messages.Add(message);
+
+        _logger.LogInformation($"Message ajouté : {message}");
+
+        return RedirectToAction("MessageForm");
+    }
+
+    [HttpGet]
+    public IActionResult GetAllMessages()
+    {
+        return View(messages);
+    }
+
+    [HttpPost]
+    public IActionResult ListStringForm(List<string> stringList)
     {
         return View();
     }
